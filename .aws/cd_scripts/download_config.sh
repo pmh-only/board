1418ca7c7bf49e18eb35d0abb286de909935a58b
Park Min Hyeok <opensource@pmh.codes>
2022-03-13 21:22:36 +0900

    chore: fix config file path

--- .aws/cd_scripts/download_config.sh
+++ .aws/cd_scripts/download_config.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-aws s3 cp s3://pmh-dev-board/uploads/settings.env .env
+aws s3 cp s3://pmh-dev-board/uploads/settings.env /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/.env
+
