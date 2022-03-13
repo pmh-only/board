bdeb028f96d0c34a4911a1e4483e6383fa2c02cb
Park Min Hyeok <opensource@pmh.codes>
2022-03-13 19:58:28 +0900

    chore: start systemd service after install

--- .aws/cd_scripts/install_dependancies.sh
+++ .aws/cd_scripts/install_dependancies.sh
@@ -3,3 +3,5 @@ yum install -y docker || exit 0
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose || exit 0
 chmod +x /usr/local/bin/docker-compose || exit 0
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose || exit 0
+
+systemctl enable --now docker
